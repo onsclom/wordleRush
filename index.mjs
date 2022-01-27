9824a8f6af8434b6419b9ea59e3ef35b1e1cd4b0
Austin Merrick <austinmerrick98@gmail.com>
2022-01-26 20:48:36 -0800

    Basic wordle functionality

--- index.mjs
+++ index.mjs
@@ -1,90 +1,128 @@
 //@ts-check
 
-const keyboardDiv = document.getElementById("keyboard");
+import {
+  makeGrid,
+  makeKeyboard,
+  makeInstructions,
+  firstTimeOnSite,
+  showInstructions,
+  seededRandom,
+  finalWords,
+  allowedWords,
+} from "./util.mjs";
 
-function keyboardButtonPressed(letter) {
-  console.log(`${letter} was pressed`);
+const gameStates = [
+  "LOADING",
+  "FIRST_WORD",
+  "PLAYING",
+  "RESULTS"
+]
+
+let state = "LOADING"
+let gameInfo = {
+  wordNum: 1,
+  curRow: 0,
+  curGuess: "",
+  curWordToGuess: "",
 }
 
-function makeKeyboard() {
-  const keyboardRows = ["QWERTYUIOP", "ASDFGHJKL", "!ZXCVBNM!"];
+function handlePressedKey(key) {
+  // ignore input if not playing
+  if (state != "PLAYING" && state != "FIRST_WORD")
+    return
 
-  let specialCount = 0;
-  for (let row of keyboardRows) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("keyboardRow");
+  if (key == "ENTER") 
+  {
+    evaluateCurrentWord()
+  }
+  else if (key == "BACK")
+  {
+    // backspace
+    if (gameInfo.curGuess.length > 0) {
+      document.getElementById(`${gameInfo.curRow},${gameInfo.curGuess.length-1}`).innerHTML = ""
+      gameInfo.curGuess = gameInfo.curGuess.slice(0, gameInfo.curGuess.length-1)
+    }
+  }
+  else
+  {
+    // letter
+    if (gameInfo.curGuess.length < 5) {
+      document.getElementById(`${gameInfo.curRow},${gameInfo.curGuess.length}`).innerHTML = key
+      gameInfo.curGuess += key
+    }
+  }
+}
 
-    for (let letter of row) {
-      let keyboardButton = document.createElement("div");
-      keyboardButton.classList.add("keyboardButton", "clickable");
-
-      if (letter == "!") {
-        letter = "BACK";
-        if (specialCount) letter = "ENTER";
-        specialCount += 1;
-        keyboardButton.classList.add("keyboardButton");
-        keyboardButton.classList.add("specialKeyboardButton");
+function evaluateCurrentWord() {
+  const curGuess = gameInfo.curGuess.toLowerCase()
+  if ( allowedWords.includes(curGuess) || finalWords.includes(curGuess)) {
+    if (state == "FIRST_WORD") {
+      state = "PLAYING"
+      startGame()
+    }
+    // go through each letter and color correctly
+    let greens = 0;
+    for (let i=0; i<5; i++) {
+      let curLetter = curGuess[i];
+      if (curLetter == gameInfo.curWordToGuess[i]) {
+        // color letter green
+        document.getElementById(`${gameInfo.curRow},${i}`).classList.add("green")
+        greens += 1;
+        continue
       }
-
-      keyboardButton.innerHTML = letter;
-      keyboardButton.id = letter;
-      keyboardButton.onclick = () => {
-        keyboardButtonPressed(letter);
-      };
-      rowDiv.appendChild(keyboardButton);
+      if (gameInfo.curWordToGuess.includes(curLetter)) {
+        document.getElementById(`${gameInfo.curRow},${i}`).classList.add("yellow")
+      }
+      else {
+        document.getElementById(`${gameInfo.curRow},${i}`).classList.add("black")
+      }
     }
 
-    keyboardDiv.appendChild(rowDiv);
+    // if all green, WIN ROW
+    if (greens == 5) {
+      // WON 
+      // TODO (clear, get new word, etc.)
+    }
+
+    if (gameInfo.curRow == 5)
+    {
+      // lost from incorrect guess
+      state = "RESULTS"
+    } 
+
+
+    gameInfo.curGuess=""
+    gameInfo.curRow+=1
+  } else {
+    // not a wordle accepted word!
   }
 }
 
-const gridDiv = document.getElementById("grid");
+function startGame() {
 
-function makeGrid() {
-  const rows = 6;
-  const wordSize = 5;
-
-  for (let i = 0; i < rows; i++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("gridRow");
-    for (let j = 0; j < wordSize; j++) {
-      let rowSquare = document.createElement("div");
-      rowSquare.classList.add("rowSquare");
-      rowSquare.id = `${i},${j}`
-      rowDiv.appendChild(rowSquare);
-    }
-    gridDiv.appendChild(rowDiv);
-  }
 }
 
-/*
-Hook up click events
-*/
+makeInstructions();
+if (firstTimeOnSite()) showInstructions();
 
-const instructions = document.getElementById("instructions");
-const instructionButton = document.getElementById("?");
+let now = new Date();
+let dateString = now.toDateString();
 
-instructions.onclick = () => {
-  instructions.classList.toggle("hide");
+// if already did today's rushle
+if (window.localStorage.getItem("lastCompletedDay") == dateString) {
+  //then show results
+  // show timer till next wrushle
+  // allow copy&paste
+} else {
+  //game time
+  makeKeyboard(handlePressedKey);
+  makeGrid();
+
+  state = "FIRST_WORD"
+
+  const randWord =
+    finalWords[Math.floor(seededRandom(dateString) * finalWords.length)];
+  gameInfo.curWordToGuess = randWord
+  console.log(randWord)
 }
 
-instructionButton.onclick = () => {
-  instructions.classList.toggle("hide");
-}
-
-/*
-Dynamically resize text grid
-*/
-
-const grid = document.getElementById("grid")
-grid.style.width = Math.min( grid.parentElement.clientWidth, grid.parentElement.clientHeight ).toString()+"px"
-grid.style.height = Math.min( grid.parentElement.clientWidth, grid.parentElement.clientHeight ).toString()+"px"
-window.onresize = () => {
-  grid.style.display = "none"
-  grid.style.width = Math.min( grid.parentElement.clientWidth, grid.parentElement.clientHeight ).toString()+"px"
-  grid.style.height = Math.min( grid.parentElement.clientWidth, grid.parentElement.clientHeight ).toString()+"px"
-  grid.style.display = "flex"
-}
-
-makeKeyboard();
-makeGrid();
